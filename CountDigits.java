b008097e32a71edac76a0630f83e9da1cc204394
Randika Nipun Bamunurathna <randikanipun326@gmail.com>
2026-05-23 11:18:42 +0530

    Add solutions for counting digits and printing a star pyramid
    1.CountDigits.java: A Java program that counts the number of digits in a given number using loops.
    
     2.PyramidStar.java: A Java program that uses nested loops to print a pyramid star pattern.

---
+++ CountDigits.java
@@ -0,0 +1,33 @@
+                // COUNT DIGITS // 
+				
+import java.util.Scanner;
+
+public class CountDigits{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        
+        int count = 0;
+        int temp = number; 
+
+      
+        if (temp == 0) {
+            count = 1;
+        } else {
+            while (temp != 0) {
+                temp = temp / 10; 
+                count++;         
+            }
+        }
+        
+        System.out.println("Number of digits in " + number + " is: " + count);
+        
+    }
+}
+
+              /* output:
+			  
+                Enter the number:1245
+				Number of digits in 1245 is 4   */
